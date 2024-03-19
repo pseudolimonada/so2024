1ee32d539c0a6ca49f47a5ca52554e983d99f196
Pedro Lima <pmc.lima@hotmail.com>
2024-03-19 18:55:56 +0000

    base commit 2

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
+#define MBUF 512
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    /* coAde */
+    char input[MBUF];
+    while(true){
+        scanf("%s",input);
+        
+        switch();
+
+
+
+
+
+    }
     return 0;
 }
