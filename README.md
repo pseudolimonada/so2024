54524390b391f3aeb89119a9b7257fd06d7455f8
Pedro Lima <pmc.lima@hotmail.com>
2024-03-19 20:03:50 +0000

    changes from 19/march class

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # so2024
 Mobile Network Manager project for Operating Systems course
+
+
+
+
+CONFIG:
+QUEUE_POS - número de slots nas filas que são utilizadas para armazenar os pedidos de autorizaçãoe os comandos dos utilizadores (>=0)
+AUTH_SERVERS_MAX - número máximo de Authorization Engines que podem ser lançados (>=1)
+AUTH_PROC_TIME - período (em ms) que o Authorization Engine demora para processar os pedidos
+MAX_VIDEO_WAIT - tempo máximo (em ms) que os pedidos de autorização do serviço de vídeo podem aguardar para serem executados (>=1)
+MAX_OTHERS_WAIT - tempo máximo (em ms) que os pedidos de autorização dos serviços de música e de redes sociais, bem como os comandos podem aguardar para serem executados (>=1)
+
+Exemplo:
+30
+6
+500
+100
+300
--- main.c
+++ main.c
@@ -1,19 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
-#define MBUF 512
+#define MBUF 1024
+
+void write_log(char *str){
+    //é preciso sincronizar com probably semaforo
+    //fara sentido ter isto num log.c para importar em todas as funcoes que vão logar
+    FILE *file = fopen("log.txt","a");
+    fwrite(str, sizeof(char), strlen(str), file);
+    fclose(file);
+    //fechar a sincronização
+    printf("%s\n",str);
+}
+
+void display_help(int options){
+    // FILE *file = fopen("menu.txt",'r');
+    // char message[MBUF];
+    // while(fgets(message,MBUF,file) != -1){
+    //     printf("%s",message);
+    // }
+    // fclose(file);
+    printf("Usage: ");
+    switch(options){
+        case 0:
+            printf(" $ mobile_user / {plafond inicial} / {número de pedidos de autorização} / {intervalo VIDEO} {intervalo MUSIC} {intervalo SOCIAL} / {dados a reservar}\n");
+            break;
+        case 1:
+            printf(" $ backoffice_user\n");
+            break;
+        case 2:
+            printf(" $ 5g_auth_platform {config-file}");
+            break;  
+    }
+}
+
+int create_manager();
+
+int create_mobile();
+
+int create_backoffice();
 
 int main(int argc, char const *argv[])
 {
-    /* coAde */
     char input[MBUF];
-    while(true){
-        scanf("%s",input);
-        
-        switch();
+    char *token;
+    int count;
+    int parser_cond;
+    int args[6];
+    int started = 0;
 
+    while(1){
+        scanf("%s",input);
+        token = strtok(input, " ");
+        count = 0;
+
+        if(strcmp(token,"backoffice_user") == 0){
+            count = 0;
+            while(token != NULL) {
+                token = strtok(NULL, " ");
+                strcpy(args[count],token);
+                count++;
+            }
+
+            if(count != 0){
+                write_log("Invalid number of args.");
+                display_help(1);
+            }
+            else{
+                create_backoffice();
+            }
+        }
+
+        else if(strcmp(token,"mobile_user") == 0){
+            count = 0;
+            while(token != NULL) {
+                token = strtok(NULL, " ");
+                args[count] = atoi(token); //ter cuidado que o atoi devolve 0 em caso de erro
+                if(args[count] <= 0){
+                    write_log("Argument is invalid. Must be a positive integer");
+                }
+                count++;
+            }
+
+            if(count != 6){
+                write_log("Invalid number of args.");
+                display_help(0);
+            }
+
+            else{
+                create_mobile(args[0],args[1],args[2],args[3],args[4],args[5]);
+            }
+        }
+        
+        else if(strcmp(token,"5g_auth_platform") == 0){
+            count = 0;
+            while(token != NULL) {
+                token = strtok(NULL, " ");
+                strcpy(args[count],token);
+                count++;
+            }
+            
+            if(count != 1){
+                write_log("Invalid number of args.");
+                display_help();
+            }
+            else if (started){
+                write_log("System manager is already running.");
+            }
+            else {
+                create_manager(args[0]);
+            }
+
+
+        }else{
+            write_log("Command doesn't exist.");
+        }
+
+        
 
 
 
